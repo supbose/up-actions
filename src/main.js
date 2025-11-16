7e3abc8b1b0588cf36a18f1df2ea41e1775c7731
supbose-home <yshxw@qq.com>
2025-11-16 11:26:29 +0800

    fix(main): 修复FTP上传时本地目录路径缺少斜杠的问题
    在uploadToFTP函数中，本地目录路径末尾添加了缺失的斜杠，
    确保文件能够正确上传到FTP服务器指定目录。

--- src/main.js
+++ src/main.js
@@ -221,7 +221,7 @@ function uploadToFTP(localDir, ftpConfig) {
             server: ftpConfig.host,
             username: ftpConfig.user,
             password: ftpConfig.password,
-            'local-dir': localDir,
+            'local-dir': localDir+'/',
             'server-dir': ftpConfig.serverDir || '/',
             exclude: [...excludeDefaults, 'dontDeployThisFolder/**']
         })
